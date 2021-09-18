a1c0d01ba69245be1b35222139efa1df885085ea
sajicode <sajioloye@gmail.com>
2021-09-18 19:48:56 +0100

    feat: use default images during loading

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 import { MovieItem, MovieItemImage } from '../styles/Movie';
 import { MovieType } from '../types';
 
-const defaultPhoto =
-  'https://res.cloudinary.com/sajicode/image/upload/v1610909420/photo_not_found.jpg';
+const defaultPhoto = '/images/movie-placeholder.png';
 
 const Movie = ({ movie: { Title, Poster } }: { movie: MovieType }) => {
+  const [loaded, setLoaded] = useState<boolean>(false);
+
   return (
     <div>
       <MovieItem>
+        {!loaded && (
+          <MovieItemImage src={defaultPhoto} alt={Title} width={300} />
+        )}
         <MovieItemImage
           src={Poster === 'N/A' ? defaultPhoto : Poster}
           alt={Title}
           width={300}
+          onLoad={() => setLoaded(true)}
         />
       </MovieItem>
     </div>
